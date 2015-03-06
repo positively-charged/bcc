positively-charged/bcc

commit 30eeb22711970d811ab4d1d9311f31397e722cea

Refactor: expression testing interface

src/expr.c
@@ -898,16 +898,18 @@ void read_format_item_array_value( struct task* task,
    }
 }
 
-void t_init_expr_test( struct expr_test* test ) {
+void t_init_expr_test( struct expr_test* test, struct stmt_test* stmt_test,
+   struct block* format_block, bool result_required, bool undef_err,
+   bool suggest_paren_assign ) {
    test->stmt_test = NULL;
    test->format_block = NULL;
    test->format_block_usage = NULL;
-   test->need_value = true;
+   test->result_required = result_required;
    test->has_string = false;
-   test->undef_err = false;
+   test->undef_err = undef_err;
    test->undef_erred = false;
    test->accept_array = false;
-   test->suggest_paren_assign = false;
+   test->suggest_paren_assign = suggest_paren_assign;
 }
 
 void init_operand( struct operand* operand ) {
@@ -934,20 +936,18 @@ void test_expr( struct task* task, struct expr_test* test, struct expr* expr,
    struct operand* operand ) {
    if ( setjmp( test->bail ) == 0 ) {
       test_node( task, test, operand, expr->root );
-      if ( operand->complete ) {
-         if ( test->need_value && ! operand->usable ) {
-            t_diag( task, DIAG_POS_ERR, &expr->pos,
-               "expression does not produce a value" );
-            t_bail( task );
-         }
-         expr->folded = operand->folded;
-         expr->value = operand->value;
-         test->pos = expr->pos;
-      }
-      else {
-         t_diag( task, DIAG_POS_ERR, &expr->pos, "expression is incomplete" );
+      if ( ! operand->complete ) {
+         t_diag( task, DIAG_POS_ERR, &expr->pos, "expression incomplete" );
          t_bail( task );
       }
+      if ( test->result_required && ! operand->usable ) {
+         t_diag( task, DIAG_POS_ERR, &expr->pos,
+            "expression does not produce a value" );
+         t_bail( task );
+      }
+      expr->folded = operand->folded;
+      expr->value = operand->value;
+      test->pos = expr->pos;
    }
 }
 
@@ -1268,10 +1268,8 @@ void test_subscript( struct task* task, struct expr_test* test,
       t_bail( task );
    }
    struct expr_test index;
-   t_init_expr_test( &index );
-   index.format_block = test->format_block;
-   index.need_value = true;
-   index.undef_err = test->undef_err;
+   t_init_expr_test( &index, test->stmt_test, test->format_block, true,
+      test->undef_err, false );
    t_test_expr( task, &index, subscript->index );
    if ( index.undef_erred ) {
       test->undef_erred = true;
@@ -1374,10 +1372,8 @@ void test_call_args( struct task* task, struct expr_test* expr_test,
    // Test remaining arguments.
    while ( ! list_end( &i ) ) {
       struct expr_test arg;
-      t_init_expr_test( &arg );
-      arg.format_block = expr_test->format_block;
-      arg.need_value = true;
-      arg.undef_err = expr_test->undef_err;
+      t_init_expr_test( &arg, expr_test->stmt_test, expr_test->format_block,
+         true, expr_test->undef_err, false );
       t_test_expr( task, &arg, list_data( &i ) );
       if ( arg.undef_erred ) {
          expr_test->undef_erred = true;
@@ -1496,20 +1492,12 @@ void t_test_format_item( struct task* task, struct format_item* item,
    struct block* format_block ) {
    while ( item ) {
       struct expr_test expr;
-      t_init_expr_test( &expr );
-      expr.stmt_test = stmt_test;
-      expr.format_block = format_block;
-      if ( expr_test ) {
-         expr.undef_err = expr_test->undef_err;
-      }
-      else {
-         expr.undef_err = true;
-      }
+      t_init_expr_test( &expr, stmt_test, format_block, false,
+         expr_test ? expr_test->undef_err : true, false );
       // When using the array cast, make acceptable an expression whose
       // result is an array.
       if ( item->cast == FCAST_ARRAY ) {
          expr.accept_array = true;
-         expr.need_value = false;
       }
       struct operand arg;
       init_operand( &arg );
@@ -1532,17 +1520,13 @@ void t_test_format_item( struct task* task, struct format_item* item,
          if ( item->extra ) {
             struct format_item_array* extra = item->extra;
             // Test offset.
-            t_init_expr_test( &expr );
-            expr.stmt_test = stmt_test;
-            expr.format_block = format_block;
-            expr.undef_err = true;
+            t_init_expr_test( &expr, stmt_test, format_block, true, true,
+               false );
             t_test_expr( task, &expr, extra->offset );
             // Test length.
             if ( extra->length ) {
-               t_init_expr_test( &expr );
-               expr.stmt_test = stmt_test;
-               expr.format_block = format_block;
-               expr.undef_err = true;
+               t_init_expr_test( &expr, stmt_test, format_block, true, true,
+                  false );
                t_test_expr( task, &expr, extra->length );
             }
          }

src/dec.c
@@ -2349,8 +2349,7 @@ void t_test_constant( struct task* task, struct constant* constant,
    }
    // Test expression.
    struct expr_test expr;
-   t_init_expr_test( &expr );
-   expr.undef_err = undef_err;
+   t_init_expr_test( &expr, NULL, NULL, true, undef_err, false );
    t_test_expr( task, &expr, constant->value_node );
    if ( ! expr.undef_erred ) {
       if ( constant->value_node->folded ) {
@@ -2390,8 +2389,7 @@ void t_test_constant_set( struct task* task, struct constant_set* set,
       }
       if ( constant->value_node ) {
          struct expr_test expr;
-         t_init_expr_test( &expr );
-         expr.undef_err = undef_err;
+         t_init_expr_test( &expr, NULL, NULL, true, undef_err, false );
          t_test_expr( task, &expr, constant->value_node );
          if ( expr.undef_erred ) {
             return;
@@ -2485,8 +2483,7 @@ void test_type_member( struct task* task, struct type_member* member,
    }
    while ( dim ) {
       struct expr_test expr;
-      t_init_expr_test( &expr );
-      expr.undef_err = undef_err;
+      t_init_expr_test( &expr, NULL, NULL, true, undef_err, false );
       t_test_expr( task, &expr, dim->size_node );
       if ( expr.undef_erred ) {
          return;
@@ -2675,8 +2672,7 @@ void test_var( struct task* task, struct var* var, bool undef_err ) {
    while ( dim ) {
       if ( dim->size_node ) {
          struct expr_test expr;
-         t_init_expr_test( &expr );
-         expr.undef_err = undef_err;
+         t_init_expr_test( &expr, NULL, NULL, true, undef_err, false );
          t_test_expr( task, &expr, dim->size_node );
          if ( expr.undef_erred ) {
             return;
@@ -2778,8 +2774,7 @@ void test_init( struct task* task, struct var* var, bool undef_err,
       if ( ! var->imported ) {
          struct value* value = ( struct value* ) var->initial;
          struct expr_test expr_test;
-         t_init_expr_test( &expr_test );
-         expr_test.undef_err = undef_err;
+         t_init_expr_test( &expr_test, NULL, NULL, true, undef_err, false );
          t_test_expr( task, &expr_test, value->expr );
          if ( expr_test.undef_erred ) {
             *undef_erred = true;
@@ -2860,8 +2855,8 @@ void test_multi_value( struct task* task, struct multi_value_test* test ) {
       else {
          struct value* value = ( struct value* ) initial;
          struct expr_test expr_test;
-         t_init_expr_test( &expr_test );
-         expr_test.undef_err = test->undef_err;
+         t_init_expr_test( &expr_test, NULL, NULL, true, test->undef_err,
+            false );
          t_test_expr( task, &expr_test, value->expr );
          if ( expr_test.undef_erred ) {
             test->undef_erred = true;
@@ -2933,8 +2928,8 @@ void test_multi_value_struct( struct task* task,
       else {
          struct value* value = ( struct value* ) initial;
          struct expr_test expr_test;
-         t_init_expr_test( &expr_test );
-         expr_test.undef_err = test->undef_err;
+         t_init_expr_test( &expr_test, NULL, NULL, true, test->undef_err,
+            false );
          t_test_expr( task, &expr_test, value->expr );
          if ( expr_test.undef_erred ) {
             test->undef_erred = true;
@@ -2988,8 +2983,7 @@ void test_func( struct task* task, struct func* func, bool undef_err ) {
       while ( param ) {
          if ( param->default_value ) {
             struct expr_test expr;
-            t_init_expr_test( &expr );
-            expr.undef_err = undef_err;
+            t_init_expr_test( &expr, NULL, NULL, true, undef_err, false );
             t_test_expr( task, &expr, param->default_value );
             if ( expr.undef_erred ) {
                break;
@@ -3042,8 +3036,7 @@ void test_func( struct task* task, struct func* func, bool undef_err ) {
       while ( param ) {
          if ( param->default_value ) {
             struct expr_test expr;
-            t_init_expr_test( &expr );
-            expr.undef_err = undef_err;
+            t_init_expr_test( &expr, NULL, NULL, true, undef_err, false );
             t_test_expr( task, &expr, param->default_value );
             if ( expr.undef_erred ) {
                return;
@@ -3085,8 +3078,7 @@ void test_func_body( struct task* task, struct func* func ) {
 void test_script( struct task* task, struct script* script ) {
    if ( script->number ) {
       struct expr_test expr;
-      t_init_expr_test( &expr );
-      expr.undef_err = true;
+      t_init_expr_test( &expr, NULL, NULL, true, true, false );
       t_test_expr( task, &expr, script->number );
       if ( ! script->number->folded ) {
          t_diag( task, DIAG_ERR | DIAG_FILE | DIAG_LINE | DIAG_COLUMN, &expr.pos,

src/stmt.c
@@ -829,8 +829,7 @@ void test_case( struct task* task, struct stmt_test* test,
       t_bail( task );
    }
    struct expr_test expr;
-   t_init_expr_test( &expr );
-   expr.undef_err = true;
+   t_init_expr_test( &expr, NULL, NULL, true, true, false );
    t_test_expr( task, &expr, label->number );
    if ( ! label->number->folded ) {
       t_diag( task, DIAG_POS_ERR, &expr.pos, "case value not constant" );
@@ -934,9 +933,7 @@ void t_test_stmt( struct task* task, struct stmt_test* test,
 void test_if( struct task* task, struct stmt_test* test,
    struct if_stmt* stmt ) {
    struct expr_test expr;
-   t_init_expr_test( &expr );
-   expr.undef_err = true;
-   expr.suggest_paren_assign = true;
+   t_init_expr_test( &expr, NULL, NULL, true, true, true );
    t_test_expr( task, &expr, stmt->cond );
    struct stmt_test body;
    t_init_stmt_test( &body, test );
@@ -950,8 +947,7 @@ void test_if( struct task* task, struct stmt_test* test,
 void test_switch( struct task* task, struct stmt_test* test,
    struct switch_stmt* stmt ) {
    struct expr_test expr;
-   t_init_expr_test( &expr );
-   expr.undef_err = true;
+   t_init_expr_test( &expr, NULL, NULL, true, true, true );
    t_test_expr( task, &expr, stmt->cond );
    struct stmt_test body;
    t_init_stmt_test( &body, test );
@@ -966,9 +962,7 @@ void test_while( struct task* task, struct stmt_test* test,
    struct while_stmt* stmt ) {
    if ( stmt->type == WHILE_WHILE || stmt->type == WHILE_UNTIL ) {
       struct expr_test expr;
-      t_init_expr_test( &expr );
-      expr.undef_err = true;
-      expr.suggest_paren_assign = true;
+      t_init_expr_test( &expr, NULL, NULL, true, true, true );
       t_test_expr( task, &expr, stmt->cond );
    }
    struct stmt_test body;
@@ -979,9 +973,7 @@ void test_while( struct task* task, struct stmt_test* test,
    stmt->jump_continue = body.jump_continue;
    if ( stmt->type == WHILE_DO_WHILE || stmt->type == WHILE_DO_UNTIL ) {
       struct expr_test expr;
-      t_init_expr_test( &expr );
-      expr.undef_err = true;
-      expr.suggest_paren_assign = true;
+      t_init_expr_test( &expr, NULL, NULL, true, true, true );
       t_test_expr( task, &expr, stmt->cond );
    }
 }
@@ -996,9 +988,7 @@ void test_for( struct task* task, struct stmt_test* test,
       struct node* node = list_data( &i );
       if ( node->type == NODE_EXPR ) {
          struct expr_test expr;
-         t_init_expr_test( &expr );
-         expr.undef_err = true;
-         expr.need_value = false;
+         t_init_expr_test( &expr, NULL, NULL, false, true, false );
          t_test_expr( task, &expr, ( struct expr* ) node );
       }
       else {
@@ -1009,18 +999,14 @@ void test_for( struct task* task, struct stmt_test* test,
    // Condition.
    if ( stmt->cond ) {
       struct expr_test expr;
-      t_init_expr_test( &expr );
-      expr.undef_err = true;
-      expr.suggest_paren_assign = true;
+      t_init_expr_test( &expr, NULL, NULL, true, true, true );
       t_test_expr( task, &expr, stmt->cond );
    }
    // Post expressions.
    list_iter_init( &i, &stmt->post );
    while ( ! list_end( &i ) ) {
       struct expr_test expr;
-      t_init_expr_test( &expr );
-      expr.undef_err = true;
-      expr.need_value = false;
+      t_init_expr_test( &expr, NULL, NULL, false, true, false );
       t_test_expr( task, &expr, list_data( &i ) );
       list_next( &i );
    }
@@ -1165,43 +1151,32 @@ void test_goto( struct task* task, struct stmt_test* test,
 void test_paltrans( struct task* task, struct stmt_test* test,
    struct paltrans* stmt ) {
    struct expr_test expr;
-   t_init_expr_test( &expr );
-   expr.undef_err = true;
+   t_init_expr_test( &expr, NULL, NULL, true, true, false );
    t_test_expr( task, &expr, stmt->number );
    struct palrange* range = stmt->ranges;
    while ( range ) {
-      t_init_expr_test( &expr );
-      expr.undef_err = true;
+      t_init_expr_test( &expr, NULL, NULL, true, true, false );
       t_test_expr( task, &expr, range->begin );
-      t_init_expr_test( &expr );
-      expr.undef_err = true;
+      t_init_expr_test( &expr, NULL, NULL, true, true, false );
       t_test_expr( task, &expr, range->end );
       if ( range->rgb ) {
-         t_init_expr_test( &expr );
-         expr.undef_err = true;
+         t_init_expr_test( &expr, NULL, NULL, true, true, false );
          t_test_expr( task, &expr, range->value.rgb.red1 );
-         t_init_expr_test( &expr );
-         expr.undef_err = true;
+         t_init_expr_test( &expr, NULL, NULL, true, true, false );
          t_test_expr( task, &expr, range->value.rgb.green1 );
-         t_init_expr_test( &expr );
-         expr.undef_err = true;
+         t_init_expr_test( &expr, NULL, NULL, true, true, false );
          t_test_expr( task, &expr, range->value.rgb.blue1 );
-         t_init_expr_test( &expr );
-         expr.undef_err = true;
+         t_init_expr_test( &expr, NULL, NULL, true, true, false );
          t_test_expr( task, &expr, range->value.rgb.red2 );
-         t_init_expr_test( &expr );
-         expr.undef_err = true;
+         t_init_expr_test( &expr, NULL, NULL, true, true, false );
          t_test_expr( task, &expr, range->value.rgb.green2 );
-         t_init_expr_test( &expr );
-         expr.undef_err = true;
+         t_init_expr_test( &expr, NULL, NULL, true, true, false );
          t_test_expr( task, &expr, range->value.rgb.blue2 );
       }
       else {
-         t_init_expr_test( &expr );
-         expr.undef_err = true;
+         t_init_expr_test( &expr, NULL, NULL, true, true, false );
          t_test_expr( task, &expr, range->value.ent.begin );
-         t_init_expr_test( &expr );
-         expr.undef_err = true;
+         t_init_expr_test( &expr, NULL, NULL, true, true, false );
          t_test_expr( task, &expr, range->value.ent.end );
       }
       range = range->next;
@@ -1226,11 +1201,7 @@ void test_packed_expr( struct task* task, struct stmt_test* test,
    struct packed_expr* packed, struct pos* expr_pos ) {
    // Test expression.
    struct expr_test expr_test;
-   t_init_expr_test( &expr_test );
-   expr_test.stmt_test = test;
-   expr_test.undef_err = true;
-   expr_test.need_value = false;
-   expr_test.format_block = packed->block;
+   t_init_expr_test( &expr_test, test, packed->block, false, true, false );
    t_test_expr( task, &expr_test, packed->expr );
    if ( expr_pos ) {
       *expr_pos = expr_test.pos;

src/task.h
@@ -891,7 +891,7 @@ struct expr_test {
    struct block* format_block;
    struct format_block_usage* format_block_usage;
    struct pos pos;
-   bool need_value;
+   bool result_required;
    bool has_string;
    bool undef_err;
    bool undef_erred;
@@ -1388,7 +1388,9 @@ void t_read_expr( struct task*, struct expr_reading* );
 bool t_is_dec( struct task* );
 void t_init_dec( struct dec* );
 void t_read_dec( struct task*, struct dec* );
-void t_init_expr_test( struct expr_test* );
+void t_init_expr_test( struct expr_test* test, struct stmt_test* stmt_test,
+   struct block* format_block, bool result_required, bool undef_err,
+   bool suggest_paren_assign );
 void t_test_expr( struct task*, struct expr_test*, struct expr* );
 void t_init_stmt_reading( struct stmt_reading*, struct list* labels );
 void t_init_stmt_test( struct stmt_test*, struct stmt_test* );

commit 6250d646cd92908efa70dfb6a1f8d2b52d376dbd

Fix regressions introduced in: 30eeb22, c73abde

src/expr.c
@@ -901,8 +901,8 @@ void read_format_item_array_value( struct task* task,
 void t_init_expr_test( struct expr_test* test, struct stmt_test* stmt_test,
    struct block* format_block, bool result_required, bool undef_err,
    bool suggest_paren_assign ) {
-   test->stmt_test = NULL;
-   test->format_block = NULL;
+   test->stmt_test = stmt_test;
+   test->format_block = format_block;
    test->format_block_usage = NULL;
    test->result_required = result_required;
    test->has_string = false;
@@ -1415,7 +1415,7 @@ void test_call_first_arg( struct task* task, struct expr_test* expr_test,
       test_call_format_arg( task, expr_test, test );
    }
    else {
-      if ( ! list_end( &test->i ) ) {
+      if ( ! list_end( test->i ) ) {
          struct node* node = list_data( test->i );
          if ( node->type == NODE_FORMAT_ITEM ) {
             struct format_item* item = ( struct format_item* ) node;