c3870f494d3978fec04ae3fe1ae28c63cd3112d1
Positron <positron@rogers.com>
2016-01-23 17:04:40 -0500

    Change the way the string buffer grows

--- src/common.c
+++ src/common.c
@@ -172,6 +172,8 @@ void mem_free_all( void ) {
 // Str
 // ==========================================================================
 
+static void adjust_buffer( struct str* string, int length );
+
 void str_init( struct str* str ) {
    str->length = 0;
    str->buffer_length = 0;
@@ -185,34 +187,42 @@ void str_deinit( struct str* str ) {
 }
 
 void str_copy( struct str* str, const char* value, int length ) {
-   if ( str->buffer_length <= length ) {
-      str_grow( str, length + 1 );
-   }
+   adjust_buffer( str, length );
    memcpy( str->value, value, length );
    str->value[ length ] = 0;
    str->length = length;
 }
 
-void str_grow( struct str* str, int length ) {
-   str->value = mem_realloc( str->value, length );
-   str->buffer_length = length;
+// If necessary, increases the buffer size enough to fit a string of the
+// specified length and the terminating null character. To select the size, an
+// initial size is doubled until the size is appropriate.
+void adjust_buffer( struct str* string, int length ) {
+   int required_length = length + 1;
+   if ( string->buffer_length < required_length ) {
+      int buffer_length = 1;
+      while ( buffer_length < required_length ) {
+         buffer_length <<= 1;
+      }
+      str_grow( string, buffer_length );
+   }
+}
+
+void str_grow( struct str* string, int length ) {
+   string->value = mem_realloc( string->value, length );
+   string->buffer_length = length;
 }
 
 void str_append( struct str* str, const char* value ) {
    int length = strlen( value );
    int new_length = str->length + length;
-   if ( str->buffer_length <= new_length ) {
-      str_grow( str, new_length + 1 );
-   }
+   adjust_buffer( str, new_length );
    memcpy( str->value + str->length, value, length );
    str->value[ new_length ] = 0;
    str->length = new_length;
 }
 
 void str_append_sub( struct str* str, const char* cstr, int length ) {
-   if ( str->buffer_length - str->length - 1 < length ) {
-      str_grow( str, str->buffer_length + length );
-   }
+   adjust_buffer( str, str->buffer_length + length );
    memcpy( str->value + str->length, cstr, length );
    str->length += length;
    str->value[ str->length ] = '\0';
