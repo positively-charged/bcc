0a36ae88b8e5cf173a0dd625554dfb766e98af6b
Positron <positron@rogers.com>
2016-06-07 00:12:46 -0400

    Rename and move several files

--- src/parse/library.c
+++ src/parse/library.c
@@ -560,19 +560,13 @@ void p_add_unresolved( struct parse* parse, struct object* object ) {
 void read_imported_libs( struct parse* parse ) {
    // Implicitly import the library that declares the dedicated and format
    // functions.
-   switch ( parse->lang ) {
-      struct import_dirc dirc;
-   case LANG_ACS:
-   case LANG_ACS95:
-      dirc.pos = parse->lib->file_pos;
-      dirc.file_path = ( parse->lang == LANG_ACS ) ?
-         "lib/acs/internals.acs" :
-         "lib/acs95/internals.acs";
-      import_lib( parse, &dirc );
-      break;
-   default:
-      break;
-   }
+   struct import_dirc dirc;
+   dirc.pos = parse->lib->file_pos;
+   dirc.file_path = ( parse->lang == LANG_ACS95 ) ?
+      "lib/acs95/internals.bcs" :
+      "lib/acs/internals.bcs";
+   import_lib( parse, &dirc );
+   // Explicit imports.
    list_iter_t i;
    list_iter_init( &i, &parse->lib->import_dircs );
    while ( ! list_end( &i ) ) {
